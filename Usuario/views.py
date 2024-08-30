60b17bbe9d23afa02daf0fad8005e09b744515eb
Xezath <cicloncita89@gmail.com>
2024-08-30 14:11:14 -0400

    fff

--- Usuario/views.py
+++ Usuario/views.py
@@ -17,9 +17,24 @@ def cerrar_sesion(request):
     return redirect ('home.html')
 
 def signin(request):
-    return render(request, 'signin.html',{
-        'form': AuthenticationForm
-    })
+    if request.method == 'GET':
+        return render(request, 'signin.html',{
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST
+            ['password'])
+        if user is None:
+            return render(request, 'signun.html',{
+                'form': AuthenticationForm,
+                'error': 'Username or password is incorrect'
+            })
+        else:
+            login(request, user)
+            return redirect ('home')
+
+     
 
 
 
