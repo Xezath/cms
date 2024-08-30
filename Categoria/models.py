849e982cf05e58d11adb397df7f7a465cf2ad261
Jaimessirve2 <jaimellave12@fpuna.edu.py>
2024-08-30 08:10:40 -0400

    avance models

--- Categoria/models.py
+++ Categoria/models.py
@@ -16,7 +16,7 @@ class Subcategoria(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField(verbose_name="Descripcion", null=True)
-    padre = models.ForeignKey(Categoria)
+    categoriaPadre = models.ForeignKey(Categoria, related_name="subcategorias", blank=True, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         fila = "Nombre: " + self.nombre
@@ -29,10 +29,10 @@ class Tipografia(models.Model):
 
 class Margenes(models.Model):   #Márgenes predefinidos
     id = models.AutoField(primary_key=True)
-    der = models.DecimalField(max_length=1000)
-    izq = models.DecimalField(max_length=1000)
-    arr = models.DecimalField(max_length=1000)
-    aba = models.DecimalField(max_length=1000)
+    der = models.DecimalField(decimal_places=2, max_digits=1000)
+    izq = models.DecimalField(decimal_places=2, max_digits=1000)
+    arr = models.DecimalField(decimal_places=2, max_digits=1000)
+    aba = models.DecimalField(decimal_places=2, max_digits=1000)
 
 class Modulos(models.Model):    #Módulos multimedia
     id = models.AutoField(primary_key=True)
@@ -43,10 +43,10 @@ class Plantilla(models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.TextField(verbose_name="Descripcion", null=True)
     colorFondo = models.IntegerField()
-    tipografia = models.ForeignKey(Margenes)
-    margenes = models.ForeignKey(Margenes)
+    tipografia = models.ForeignKey(Tipografia, on_delete=models.CASCADE)
+    margenes = models.ForeignKey(Margenes, on_delete=models.CASCADE)
     disposicion = models.BooleanField() #Horizonal o Vertical
-    modulos = models.ForeignKey(Modulos)
+    modulos = models.ForeignKey(Modulos, on_delete=models.CASCADE)
 
     def __str__(self):
         fila = "Nombre: " + self.nombre
