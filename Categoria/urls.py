24a213dc1d707727d6f675fef6c8018f937b3212
Jaimessirve2 <jaimellave12@fpuna.edu.py>
2024-08-30 06:22:10 -0400

    avance en categorias

--- Categoria/urls.py
+++ Categoria/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.cat, name='cat'),
+    path('', views.inicio, name='inicio'),
+    path('categorias', views.categorias, name='categorias'),
+    path('categorias/crear', views.crear, name='crear'),
+    path('categorias/editar', views.editar, name='editar')
 ]
--- Categoria/views.py
+++ Categoria/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def cat(request):
-    return render(request, 'paginas/index.html')
+def inicio(request):
+    return render(request, 'paginas/inicio.html')
+def categorias(request):
+    return render(request, 'categorias/index.html')
+def crear(request):
+    return render(request, 'categorias/crear.html')
+def editar(request):
+    return render(request, 'categorias/editar.html')
