414e13016fcae8e4866e55d0bcf9b387df5ef575
Jaimessirve2 <jaimellave12@fpuna.edu.py>
2024-08-30 08:58:17 -0400

    cree vistas para subcategoria

--- Categoria/urls.py
+++ Categoria/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('categorias', views.categorias, name='categorias'),
     path('categorias/crear', views.crear, name='crear'),
-    path('categorias/editar', views.editar, name='editar')
+    path('categorias/editar', views.editar, name='editar'),
+    #Subcategorias
+    path('subcategorias', views.subcategorias, name='subcategorias'),
+    path('subcategorias/crear', views.crear_sub, name='crear_sub'),
+    path('subcategorias/editar', views.editar_sub, name='editar_sub')
 ]
--- Categoria/views.py
+++ Categoria/views.py
@@ -13,3 +13,13 @@ def crear(request):
     return render(request, 'categorias/crear.html')
 def editar(request):
     return render(request, 'categorias/editar.html')
+
+#Subcategorias
+def subcategorias(request):
+    #subcategorias = Subcategorias.objects.all()
+    #print(categorias)
+    return render(request, 'subcategorias/index.html', {'subcategorias': subcategorias})
+def crear_sub(request):
+    return render(request, 'subcategorias/crear.html')
+def editar_sub(request):
+    return render(request, 'subcategorias/editar.html')
