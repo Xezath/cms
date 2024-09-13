661ab66a4bc0617b4e8f3cb6014a555dc2d840d9
Jaimessirve2 <jaimellave12@fpuna.edu.py>
2024-09-13 12:19:38 -0400

    aaaaa

--- cms/settings.py
+++ cms/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.github',
+    'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.openid_connect',
     "crispy_forms",
     "crispy_bootstrap5",
