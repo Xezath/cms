231c6b8d2648d69617687d3d6ba22d2e8f6d1aa6
pazsanchezs <pachiisalinas23@fpuna.edu.py>
2024-09-24 11:36:27 -0400

    +opciones de filtro

--- Contenidos/views.py
+++ Contenidos/views.py
@@ -3,16 +3,46 @@ from Categoria.models import Categoria, Subcategoria
 from .forms import ContenidosForm, EditarContenidosForm, VisualizarContenidoForm
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from django.contrib.auth.decorators import permission_required
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import permission_required, login_required
 from django.http import JsonResponse
+from django.contrib.auth.models import User  # Asegúrate de importar User
 
-@permission_required('Contenidos.view_contenidos',raise_exception=True)
+@permission_required('Contenidos.view_contenidos', raise_exception=True)
 def contenidos(request):
-    contenidos = Contenidos.objects.all()
-    return render(request, 'contenidos/contenidos.html', {'contenidos': contenidos})
+    """
+    Vista que muestra una lista de todos los contenidos disponibles
+    y permite filtrarlos por categoría, subcategoría o autor.
+    """
+    # Obtener los parámetros de filtrado de la solicitud
+    categoria_id = request.GET.get('categoria')
+    subcategoria_id = request.GET.get('subcategoria')
+    autor_id = request.GET.get('autor')
 
-@permission_required('Contenidos.add_contenidos',raise_exception=True)
+    # Filtrar contenidos basado en los parámetros
+    contenidos = Contenidos.objects.all()
+
+    if categoria_id:
+        contenidos = contenidos.filter(categoria_id=categoria_id)
+    
+    if subcategoria_id:
+        contenidos = contenidos.filter(subcategoria_id=subcategoria_id)
+    
+    if autor_id:
+        contenidos = contenidos.filter(autor_id=autor_id)
+
+    # Obtener listas para los filtros
+    categorias = Categoria.objects.all()
+    subcategorias = Subcategoria.objects.all()
+    autores = User.objects.all()  # Asegúrate de que puedes acceder a los usuarios
+
+    return render(request, 'contenidos/contenidos.html', {
+        'contenidos': contenidos,
+        'categorias': categorias,
+        'subcategorias': subcategorias,
+        'autores': autores
+    })
+
+@permission_required('Contenidos.add_contenidos', raise_exception=True)
 def crear_contenido(request):
     formulario = ContenidosForm(request.POST or None)
     if formulario.is_valid():
@@ -21,7 +51,7 @@ def crear_contenido(request):
         return redirect('contenidos')  
     return render(request, 'contenidos/crear.html', {'formulario': formulario})
 
-@permission_required('Contenidos.change_contenidos',raise_exception=True)
+@permission_required('Contenidos.change_contenidos', raise_exception=True)
 def editar_contenido(request, id):
     contenido = get_object_or_404(Contenidos, id=id)
 
@@ -43,8 +73,7 @@ def editar_contenido(request, id):
 
     return render(request, 'contenidos/editar.html', {'formulario': formulario})
 
-
-@permission_required('Contenidos.delete_contenidos',raise_exception=True)
+@permission_required('Contenidos.delete_contenidos', raise_exception=True)
 def eliminar_contenido(request, pk):
     contenido = get_object_or_404(Contenidos, pk=pk)
     if request.method == 'POST':
@@ -53,13 +82,13 @@ def eliminar_contenido(request, pk):
         return redirect('contenidos')
     return render(request, 'contenidos/confirmar_eliminacion.html', {'contenido': contenido})
 
-@permission_required('Contenidos.view_contenidos',raise_exception=True)
+@permission_required('Contenidos.view_contenidos', raise_exception=True)
 def visualizar_contenido(request, id):
     contenido = Contenidos.objects.get(id=id)
-    formulario = VisualizarContenidoForm(request.POST or None,instance=contenido)
-    if formulario.is_valid and request.POST:
+    formulario = VisualizarContenidoForm(request.POST or None, instance=contenido)
+    if formulario.is_valid() and request.POST:
         formulario.save()
-    return render(request, 'contenidos/visualizar.html',{'formulario': formulario, 'contenido':contenido})
+    return render(request, 'contenidos/visualizar.html', {'formulario': formulario, 'contenido': contenido})
 
 def cargar_subcategorias(request):
     categoria_id = request.GET.get('categoria_id')
