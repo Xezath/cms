8b293ffa986ee8522b48428afa9d1c0a9fdc5b6d
pazsanchezs <pachiisalinas23@fpuna.edu.py>
2024-09-24 10:29:02 -0400

    +subcategorias y autor

--- Contenidos/forms.py
+++ Contenidos/forms.py
@@ -1,43 +1,87 @@
 from django import forms
 from ckeditor.widgets import CKEditorWidget
-from Categoria.models import Categoria
+from Categoria.models import Categoria, Subcategoria
 from Plantilla.models import Plantilla
 from .models import Contenidos
 
 class ContenidosForm(forms.ModelForm):
     categoria = forms.ModelChoiceField(
-        queryset=Categoria.objects.all(),  # Aquí defines el queryset con las opciones
-        widget=forms.Select,               # Esto asegura que se renderice como un <select>
-        empty_label="Seleccione una categoría",  # Etiqueta para el valor vacío
-        label="Categoría"            # Cambia la etiqueta si lo necesitas
+        queryset=Categoria.objects.all(),
+        widget=forms.Select(attrs={'id': 'categoria-select'}),
+        empty_label="Seleccione una categoría",
+        label="Categoría"
+    )
+    subcategoria = forms.ModelChoiceField(
+        queryset=Subcategoria.objects.none(),
+        widget=forms.Select(attrs={'id': 'subcategoria-select'}),
+        empty_label="Seleccione una subcategoría",
+        required=False,
+        label="Subcategoría"
     )
     plantilla = forms.ModelChoiceField(
-        queryset=Plantilla.objects.all(), 
+        queryset=Plantilla.objects.all(),
         required=False,
-        widget=forms.Select,               # Esto asegura que se renderice como un <select>
-        empty_label="Seleccione una plantilla",  # Etiqueta para el valor vacío
+        widget=forms.Select,
+        empty_label="Seleccione una plantilla",
         label="Plantilla"
-        )
-    class Meta:
-        model = Contenidos
-        fields = '__all__'  
-class EditarContenidosForm(forms.ModelForm):
-    categoria = forms.ModelChoiceField(
-        queryset=Categoria.objects.all(),  # Aquí defines el queryset con las opciones
-        widget=forms.Select,               # Esto asegura que se renderice como un <select>
-        empty_label="Seleccione una categoría",  # Etiqueta para el valor vacío
-        label="Categoría"            # Cambia la etiqueta si lo necesitas
     )
-    plantilla = forms.ModelChoiceField(
-        queryset=Plantilla.objects.all(), 
-        required=False,
-        widget=forms.Select,               # Esto asegura que se renderice como un <select>
-        empty_label="Seleccione una plantilla",  # Etiqueta para el valor vacío
-        label="Plantilla"
-        )
+
     class Meta:
         model = Contenidos
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Verificar si hay una categoría seleccionada en los datos enviados
+        if 'categoria' in self.data:
+            try:
+                categoria_id = int(self.data.get('categoria'))
+                self.fields['subcategoria'].queryset = Subcategoria.objects.filter(categoriaPadre_id=categoria_id).order_by('nombre')
+            except (ValueError, TypeError):
+                self.fields['subcategoria'].queryset = Subcategoria.objects.none()
+        else:
+            # Si no hay datos enviados o es una nueva instancia
+            self.fields['subcategoria'].queryset = Subcategoria.objects.none()
+
+
+class EditarContenidosForm(forms.ModelForm):
+    categoria = forms.ModelChoiceField(
+        queryset=Categoria.objects.all(),
+        widget=forms.Select(attrs={'id': 'categoria-select'}),
+        empty_label="Seleccione una categoría",
+        label="Categoría"
+    )
+    subcategoria = forms.ModelChoiceField(
+        queryset=Subcategoria.objects.none(),
+        widget=forms.Select(attrs={'id': 'subcategoria-select'}),
+        empty_label="Seleccione una subcategoría",
+        required=False,
+        label="Subcategoría"
+    )
+    plantilla = forms.ModelChoiceField(
+        queryset=Plantilla.objects.all(), 
+        required=False,
+        widget=forms.Select,
+        empty_label="Seleccione una plantilla",
+        label="Plantilla"
+    )
+
+    class Meta:
+        model = Contenidos
+        fields = '__all__'
+ 
+        def __init__(self, *args, **kwargs):
+            
+            super().__init__(*args, **kwargs)
+            if 'categoria' in self.data:
+                try:
+                    categoria_id = int(self.data.get('categoria'))
+                    self.fields['subcategoria'].queryset = Subcategoria.objects.filter(categoriaPadre_id=categoria_id).order_by('nombre')
+                except (ValueError, TypeError):
+                    pass
+            elif self.instance.pk and self.instance.categoria:
+                self.fields['subcategoria'].queryset = Subcategoria.objects.filter(categoria=self.instance.categoria).order_by('nombre')
+
 
 class VisualizarContenidoForm(forms.ModelForm):
     plantilla = forms.ModelChoiceField(
@@ -50,3 +94,13 @@ class VisualizarContenidoForm(forms.ModelForm):
     class Meta:
         model = Contenidos
         fields = '__all__'
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if 'categoria' in self.data:
+            try:
+                categoria_id = int(self.data.get('categoria'))
+                self.fields['subcategoria'].queryset = Subcategoria.objects.filter(categoria_id=categoria_id).order_by('nombre')
+            except (ValueError, TypeError):
+                pass  # Valor inválido de categoría, ignora y usa el queryset vacío
+        elif self.instance.pk:
+            self.fields['subcategoria'].queryset = self.instance.categoria.subcategorias.order_by('nombre')
--- Contenidos/views.py
+++ Contenidos/views.py
@@ -1,9 +1,11 @@
 from .models import Contenidos
+from Categoria.models import Categoria, Subcategoria
 from .forms import ContenidosForm, EditarContenidosForm, VisualizarContenidoForm
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 
 @permission_required('Contenidos.view_contenidos',raise_exception=True)
 def contenidos(request):
@@ -14,18 +16,33 @@ def contenidos(request):
 def crear_contenido(request):
     formulario = ContenidosForm(request.POST or None)
     if formulario.is_valid():
+        contenidos.autor = request.user
         formulario.save()
         return redirect('contenidos')  
     return render(request, 'contenidos/crear.html', {'formulario': formulario})
 
 @permission_required('Contenidos.change_contenidos',raise_exception=True)
 def editar_contenido(request, id):
-    contenido = Contenidos.objects.get(id=id)
-    formulario = EditarContenidosForm(request.POST or None,instance=contenido)
-    if formulario.is_valid and request.POST:
-        formulario.save()
-        return redirect('contenidos')
-    return render(request, 'contenidos/editar.html',{'formulario': formulario, 'contenido':contenido})
+    contenido = get_object_or_404(Contenidos, id=id)
+
+    if request.method == 'POST':
+        formulario = EditarContenidosForm(request.POST, instance=contenido)
+        
+        # Actualiza el queryset de subcategorías basadas en la categoría seleccionada
+        categoria_id = request.POST.get('categoria')
+        if categoria_id:
+            formulario.fields['subcategoria'].queryset = Subcategoria.objects.filter(categoriaPadre_id=categoria_id)
+
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('contenidos')
+        else:
+            print(formulario.errors)  # Muestra los errores en la consola para depuración
+    else:
+        formulario = EditarContenidosForm(instance=contenido)
+
+    return render(request, 'contenidos/editar.html', {'formulario': formulario})
+
 
 @permission_required('Contenidos.delete_contenidos',raise_exception=True)
 def eliminar_contenido(request, pk):
@@ -43,3 +60,14 @@ def visualizar_contenido(request, id):
     if formulario.is_valid and request.POST:
         formulario.save()
     return render(request, 'contenidos/visualizar.html',{'formulario': formulario, 'contenido':contenido})
+
+def cargar_subcategorias(request):
+    categoria_id = request.GET.get('categoria_id')
+
+    # Verificar si categoria_id es válido
+    if categoria_id and categoria_id.isdigit():
+        subcategorias = Subcategoria.objects.filter(categoriaPadre_id=int(categoria_id)).all()
+    else:
+        subcategorias = []  # Si no hay una categoría válida, devolver una lista vacía.
+
+    return JsonResponse(list(subcategorias.values('id', 'nombre')), safe=False)
