eb3130670bc7ae16c9aeb00a8984e96ef16a3bf8
Xezath <cicloncita89@gmail.com>
2024-10-03 12:02:45 -0400

    cambie la bd

--- cms/settings.py
+++ cms/settings.py
@@ -182,9 +182,9 @@ WSGI_APPLICATION = 'cms.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cms_prueba',
+        'NAME': 'cms',
         'USER': 'postgres',  
-        'PASSWORD': 'password',
+        'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '5432',  # puerto por defecto de PostgreSQL
     }
