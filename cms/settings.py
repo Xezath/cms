1bd5f1a542e3002232ddfcbfea1a69a6fc5d1309
JazminIrazusta <jazminirazustafrutos@fpuna.edu.py>
2024-10-18 14:53:30 -0300

    Kanban

--- cms/settings.py
+++ cms/settings.py
@@ -177,9 +177,9 @@ WSGI_APPLICATION = 'cms.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tester',
+        'NAME': 'cms',
         'USER': 'postgres',  
-        'PASSWORD': 'admin',
+        'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '5432',  # puerto por defecto de PostgreSQL
     }
