9c3689d2f3b487543c8aa3bb0c990a049fce0aad
Jaimessirve2 <jaimellave12@fpuna.edu.py>
2024-10-24 15:59:05 -0300

    cambio de nombre aceptado_rev.html

--- Contenidos/views.py
+++ Contenidos/views.py
@@ -287,7 +287,7 @@ def visualizar_contenido_aceptado(request, id):
     """
     contenido = get_object_or_404(Contenidos, id=id)
 
-    return render(request, 'contenidos/aceptado.html', {
+    return render(request, 'contenidos/aceptado_rev.html', {
         'contenido': contenido,
 })
 
