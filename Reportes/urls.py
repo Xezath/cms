68b3057468ef72ddc893b210da7da2499769aeb4
Xezath <cicloncita89@gmail.com>
2024-11-07 17:10:02 -0300

    agregue el path para tiempo de revision

--- Reportes/urls.py
+++ Reportes/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     
     path('contenidos_mas_leidos/', views.reporte_contenidos_mas_leidos, name='reporte_contenidos_mas_leidos'),
     path('contenidos_publicados_rechazados/', views.reporte_contenidos_publicados_rechazados, name='reporte_contenidos_publicados_rechazados'),
+    path('reporte-promedio-tiempo-revision/', views.reporte_promedio_tiempo_revision, name='reporte_promedio_tiempo_revision'),
 ]
